b6939c8ac45282b55f34027bd917bf4d60fde2ff
Luke Garrison <lgarriso@nd.edu>
2016-11-08 13:45:45 -0500

    Initial commit

---
+++ client/myfrm.cpp
@@ -0,0 +1,11 @@
+/*
+ * Luke Garrison, Nick Ward
+ * lgarriso, nward3
+ *
+ * client application for message board forum
+ */
+
+int main() {
+
+	return 0;
+}
---
+++ server/myfrmd.cpp
@@ -0,0 +1,11 @@
+/*
+ * Luke Garrison, Nick Ward
+ * lgarriso, nward3
+ *
+ * server side for message board forum
+ */
+
+int main() {
+
+	return 0;
+}
